b9db8cc474c6bc37960004fbed76ce283e090dc9
JSaumat <j_saumat@mail.fhsu.edu>
2025-02-07 19:08:51 -0700

    Added matplotlib. Converted close cost to a numpy array. Created graphs and show them.

--- main.py
+++ main.py
@@ -4,6 +4,17 @@
 
 import pprint
 import yfinance as yf
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+#get today's date
+today = datetime.now()
+
+#Calculate the date 10 days ago
+
+ten_days_ago = today - timedelta(days=14)
 
 mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
 
@@ -14,18 +25,35 @@ mytickers.sort()
 for ticker in mytickers:
 
     result = yf.Ticker(ticker)
-    mydata[ticker] = {'ticket': ticker, 'dayHigh': result.info['dayHigh']}
+    hist = result.history(start=ten_days_ago, end=today)
 
-    print(f"Ticker: {ticker} \tDay High: {result.info['dayHigh']}")
+    last10days = []
 
-#aapl = yf.Ticker("AAPL")
+    for date in hist['Close'][:10]:
 
-#print(aapl.info["dayHigh"])
+        last10days.append(date)
 
-#get all stock info msft.info
-#pprint.pprint(aapl.info)
+    if len(last10days) == 10:
 
-#get historical market data
-#hist = msft.history(period="1mo")
+        # maxlist = copy.copy(last10days)
+        # maxlist.sort()
+        # max_price = maxlist[-1]+10
+        # min_price = maxlist[0]-10
 
-#pprint.pprint(hist)
+
+        # max_price = np.max(last10days)
+        # min_price = np.min(last10days)
+
+
+        myarray = np.array(last10days)
+        max_price = myarray.max() + (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.min()*.05)
+        plt.plot(myarray)
+        plt.xlabel('Days Ago')
+        plt.ylabel('Closing Price')
+        plt.axis((9, 0, min_price, max_price))
+        plt.title(f"{ticker} Last 10 Closing Prices")
+        plt.show()
+
+    else:
+        print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
