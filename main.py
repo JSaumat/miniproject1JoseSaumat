22b48984f6f217949143fba3003a5f27e6a8424f
JSaumat <j_saumat@mail.fhsu.edu>
2025-02-11 21:46:13 -0700

    Added comments to the code to explain what the code is doing.

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 ### Jose Saumat
 ### Mini Project 1
 
+#Imports the required packages
 import yfinance as yf
 from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# Creates the charts folder to hold the charts created by the program
 os.makedirs("charts", exist_ok=True)
 
 #get today's date
@@ -15,28 +17,38 @@ today = datetime.now()
 
 #Calculate the date 10 days ago
 
+# Variable that is using time delta from datetime package to get the last 10 days of stock market data
 ten_days_ago = today - timedelta(days=14)
 
+# List of all the stocks being used in this mini project
 mytickers = ["MSFT", "AAPL", "NVDA", "GME", "AMC"]
 
+# Dictionary called mydata
 mydata = {}
 
+# Cycles through all stock tickers in mytickers list
 for ticker in mytickers:
 
+    # Variables using Yahoo Finance to get stock ticker information
     result = yf.Ticker(ticker)
     hist = result.history(start=ten_days_ago, end=today)
 
+    # List for the last 10 days of data gathered
     last10days = []
 
+    # Cycles through the close dates for the last 10 days
     for date in hist['Close'][:10]:
 
+        # Appends the dates to the list of 10 days for each ticker
         last10days.append(date)
 
+    # Check to make sure we get 10 data points
     if len(last10days) == 10:
 
         # max_price = np.max(last10days)
         # min_price = np.min(last10days)
 
+        # Uses matplotlib to create the graphs and the information they display
         myarray = np.array(last10days)
         max_price = myarray.max() + (myarray.max()*.05)
         min_price = myarray.min() - (myarray.min()*.05)
@@ -50,5 +62,6 @@ for ticker in mytickers:
         #Provides user with the 5 graphs as a pop-up after using terminal
         #plt.show()
 
+    # Message that prints if we do not have 10 days worth of data as required by the specification
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
